bb9a39c49bedfc5b411ce4e2400908a456dbc7d5
eugeniacotrim <eugeniacotrim@gmail.com>
2024-01-28 06:02:52 -0800

    second commit

--- src/test/java/com/test/Addition.java
+++ src/test/java/com/test/Addition.java
@@ -1,18 +1,20 @@
 package com.test;
 
 import org.testng.annotations.Test;
+
+import com.dev.LibMath;
+
 import static org.testng.Assert.assertEquals;
 
 public class Addition {
   @Test
   public void testAdd() {
-	int num1 = 5;
-	int num2 = 4;
-	int actSum = num1+ num2;
-	int expSum = 9;
-	
-	assertEquals(actSum, expSum);
-	System.out.println("Sum is: " +actSum);
+	  LibMath calc = new LibMath();
+	  int actSum = calc.Sum(10, 20);
+	  int expSum = 30;
+	  
+	  assertEquals(actSum, expSum);
+	  System.out.println("Sum is: " +actSum);
   }
   
 }
